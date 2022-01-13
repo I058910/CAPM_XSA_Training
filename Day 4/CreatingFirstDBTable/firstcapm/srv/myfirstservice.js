557c3a01a371a0561645210a2306ccac2b9770a5
chandrashekharvedi <cvedi@soyuztechnologies.com>
2022-01-13 16:11:41 +0530

    Adjustments

--- Day 4/CreatingFirstDBTable/firstcapm/srv/myfirstservice.js
+++
@@ -1,6 +0,0 @@
-const myfirstservice = function(srv) {
-    srv.on('hello', (req) => {
-        return  "Hello " + req.data.to + "!";
-    });
-}
-module.exports = myfirstservice
--- Day 5/StaticLocalizationPart1/firstcapm/srv/CatalogService.cds
+++
@@ -1,5 +0,0 @@
-using { anubhav } from '../db/datamodel';
-
-service CatalogService{
-    entity BusinessPartner as projection on anubhav.db.master.businesspartner;
-}
